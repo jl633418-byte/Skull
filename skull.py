b38310b08bd9f316c78762cb436b885d508c842b
jl633418-byte <jl633418@gmail.com>
2026-08-24 15:32:24 -0700

    Use environment variable for Discord token

--- skull.py
+++ skull.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import random
 import datetime
-
+import os
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True  # necesario para algunos comandos
@@ -262,4 +262,4 @@ async def iq(ctx):
     await ctx.send(f"Mi IQ actual es: **{iq_falso}**\n(confirmado: soy un retrasado)")
 
 # ==================== EJECUTAR ====================
-bot.run("TU_TOKEN_AQUÍ")
+bot.run(os.getenv("DISCORD_TOKEN"))
